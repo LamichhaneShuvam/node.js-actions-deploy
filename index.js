3bc44ab06c05a059441bb6578dc6ed6185773d16
Shuvam Lamichhane <shuvamlamichhane@gmail.com>
2022-09-14 17:16:21 +0545

    express app init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3006;
+const uuid = require('uuid');
+
+app.get('/', (req, res) => {
+  res.send(uuid.v4())
+})
+
+app.listen(PORT, () => console.log(`Server started on port -> ${PORT}`));
